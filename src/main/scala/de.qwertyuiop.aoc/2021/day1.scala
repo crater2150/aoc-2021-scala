a9ec0217b1d13369fb149f07852d0f7d41b4e203
Alexander Gehrke <github@qwertyuiop.de>
2021-12-02 23:35:42 +0100

    day1

--- src/main/scala/de.qwertyuiop.aoc/2021/day1.scala
+++ src/main/scala/de.qwertyuiop.aoc/2021/day1.scala
@@ -3,4 +3,15 @@ package de.qwertyuiop.aoc.`2021`
 import de.qwertyuiop.aoc.lib.*
 import cats.*, cats.implicits.given
 
-def day1(using InputSource): Unit = ???
+def day1(using InputSource): Unit =
+  val numbers = input(_.toInt)
+  println(countIncreases(numbers))
+  println(countIncreasesSmoothed(numbers, 3))
+
+/* part 1 */
+def countIncreases(input: List[Int]): Int = input.sliding(2).count(l => l(0) < l(1))
+
+/* part 2 */
+def countIncreasesSmoothed(input: List[Int], smoothingWindow: Int) =
+  countIncreases(input.sliding(smoothingWindow).map(_.sum).toList)
+
