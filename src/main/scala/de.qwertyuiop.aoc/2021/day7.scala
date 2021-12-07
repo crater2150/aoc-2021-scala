8ea208920a5fce695b598e9052cbd9963b668f11
Alexander Gehrke <github@qwertyuiop.de>
2021-12-07 16:03:54 +0100

    day 7

--- src/main/scala/de.qwertyuiop.aoc/2021/day7.scala
+++ src/main/scala/de.qwertyuiop.aoc/2021/day7.scala
@@ -2,5 +2,15 @@ package de.qwertyuiop.aoc.`2021`
 
 import de.qwertyuiop.aoc.lib.*
 import cats.*, cats.implicits.given
+import util.chaining.given
 
-def day7(using InputSource): Unit = ???
+def day7(using InputSource): Unit =
+  val positions = input(_.splitNN(",").map(_.toInt).toVector).head
+
+  def minCost(positions: Iterable[Int])(cost: (Int, Int) => Int) =
+    (positions.min to positions.max)
+      .map(target => (target, positions.map(start => cost(start, target)).sum))
+      .minBy(_._2)
+
+  minCost(positions)((s, t) => (s - t).abs).pipe(println)
+  minCost(positions)((s, t) => { val diff = (s - t).abs; diff * (diff + 1) / 2 }).pipe(println)
