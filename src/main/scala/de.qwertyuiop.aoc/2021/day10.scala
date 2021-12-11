be7e1d14de6f593e4a5f07b839195dd24e329f0f
Alexander Gehrke <github@qwertyuiop.de>
2021-12-12 00:40:27 +0100

    Day 10

--- src/main/scala/de.qwertyuiop.aoc/2021/day10.scala
+++ src/main/scala/de.qwertyuiop.aoc/2021/day10.scala
@@ -3,4 +3,42 @@ package de.qwertyuiop.aoc.`2021`
 import de.qwertyuiop.aoc.lib.*
 import cats.*, cats.implicits.given
 
-def day10(using InputSource): Unit = ???
+def day10(using InputSource): Unit =
+  val source = input()
+  val opposites = Map(
+    ')' -> '(', ']' -> '[', '}' -> '{', '>' -> '<',
+    '(' -> ')', '[' -> ']', '{' -> '}', '<' -> '>'
+  )
+  val errorPoints = Map(
+    ')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137,
+  )
+  val correctionPoints = Map(
+    ')' -> 1L, ']' -> 2L, '}' -> 3L, '>' -> 4L,
+  )
+
+  def validate(line: String) =
+    def step(open: List[Char], remain: List[Char]): Either[Char, List[Char]] =
+      (open, remain) match 
+        case (o, (next @ ('('|'['|'{'|'<')) :: rest)  => step(next :: o, rest)
+        case (lastOpen :: tailOpen, (next @ (')'|']'|'}'|'>')) :: rest)  =>
+          if opposites(next) == lastOpen then
+            step(tailOpen, rest)
+          else
+            Left(next)
+        case (o, Nil) => Right(o)
+        case (o, illegalChar:: r) => Left(illegalChar)
+
+    step(List(), line.toList)
+
+  val checked = source.map(validate)
+  val totalErrorPoints = checked.collect{ case Left(c) => errorPoints(c)}.sum
+  val completionsForValid = checked.collect{ case Right(open) => open.map(opposites andThen correctionPoints)}
+  val allCompletionPoints = completionsForValid.map(_.foldLeft(0L)((total, next) => total * 5L + next)).sorted
+  println("All Points:")
+  allCompletionPoints.foreach(println)
+  println("---")
+  val middleCompletionPoints = allCompletionPoints(allCompletionPoints.size / 2)
+
+  println(totalErrorPoints)
+  println(middleCompletionPoints)
+
